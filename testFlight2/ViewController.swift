cf0702074571d82f19c7a4bad6516db48ffc62c0
Yujin Yano <northwindapps@gmail.com>
2020-10-26 17:41:27 +0900

    Initial Commit

---
+++ testFlight2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  testFlight2
+//
+//  Created by yujin on 2020/10/26.
+//  Copyright © 2020 yujin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
